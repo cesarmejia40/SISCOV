e0778904eb1d133dc8b8e324cc2967c94da2a88f
desarolloguma <analista.guma@gmail.com>
2017-06-30 14:32:11 -0600

    Cambio 30-06-2017

--- application/views/Reportes/RMenu.php
+++ application/views/Reportes/RMenu.php
@@ -9,61 +9,22 @@
         <?php
             if (!isset($_SESSION['Permiso'])) {
             }else{
-                switch ($_SESSION['Permiso']) {
-                    case 1: //ADMINISTRADOR
-                        echo '<div class="col offset-l3 s12 l2">
-                                    <div class="card small" >
-                                        <a href='.base_url("index.php/CalSemana").'>
-                                            <div class="card-image">
-                                                <img style="margin-top:20px;" src='.base_url("assets/img/reporte/CalSemana.png").' >
-                                            </div>
-                                            <div class="card-content center">
-                                                <p style="color:black;">CALENDARIO</p>
-                                            </div>
-                                        </a>
-                                    </div>
-                              </div>';
-                    break;
-                    /*case 2: //DIGITADOR
-                        echo '<div class="col offset-l3 s12 l2">
-                                    <div class="card small" >
-                                        <a href='.base_url("index.php/Trabajadores").'>
-                                            <div class="card-image">
-                                                <img style="margin-top:20px;" src='.base_url("assets/img/menu/reportes.jpg").' >
-                                            </div>
-                                            <div class="card-content center">
-                                                <p style="color:black;">CALENDARIO</p>
-                                            </div>
-                                        </a>
-                                    </div>
-                              </div>
-
-                              <div class="col s12 l2">
-                                    <div class="card small" >
-                                        <a href='.base_url("index.php/Reportes").'>
-                                            <div class="card-image">
-                                                <img style="margin-top:7px;" src='.base_url("assets/img/menu/reportes.jpg").' >
-                                            </div>
-                                            <div class="card-content center">
-                                                <p style="color:black;">VISUALIZAR REPORTES</p>
-                                            </div>
-                                        </a>
-                                    </div>
-                              </div>
-                              <div class="col s12 l2">
-                                    <div class="card small" >
-                                        <a href='.base_url("index.php/Reportes").'>
-                                            <div class="card-image">
-                                                <img style="margin-top:7px;" src='.base_url("assets/img/menu/reportes.jpg").' >
-                                            </div>
-                                            <div class="card-content center">
-                                                <p style="color:black;">VISUALIZAR REPORTES</p>
-                                            </div>
-                                        </a>
-                                    </div>
-                              </div>';
-                    break;*/
+                if(($_SESSION['Permiso'] == 1) OR ($_SESSION['Permiso'] == 2)){
+                    $csql = '<div class="col offset-l3 s12 l2">
+                                <div class="card small" >
+                                    <a href='.base_url("index.php/CalSemana").'>
+                                        <div class="card-image">
+                                            <img style="margin-top:20px;" src='.base_url("assets/img/reporte/CalSemana.png").' >
+                                        </div>
+                                        <div class="card-content center">
+                                            <p style="color:black;">CALENDARIO</p>
+                                        </div>
+                                    </a>
+                                </div>
+                             </div>';
                 }
+
+                echo $csql;
             }
         ?> 
     </div>
